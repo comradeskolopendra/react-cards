cac0017741e35ca8f482fcd757359416407928b5
comradeskolopendra <91865240+comradeskolopendra@users.noreply.github.com>
2022-04-26 11:42:05 +0600

    Add files via upload

--- src/components 2/Swiper.jsx
+++ src/components 2/Swiper.jsx
@@ -1,33 +1,40 @@
 // import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y, EffectFade } from 'swiper';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+// import { useSwiper } from 'swiper/react';
+// import { EffectFade } from 'swiper';
 
 // Import Swiper styles
-import 'swiper/css';
 import styles from "../styles/swiper.module.css"
-// import '../styles'
+import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 // import 'swiper/css/scrollbar';
 
 export default function SwiperTest() {
   return (
-    <Swiper className={styles.containerSwiper}
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide className={styles.swiperSlide}>Slide 1</SwiperSlide>
-      <SwiperSlide className={styles.swiperSlide}>Slide 2</SwiperSlide>
-      <SwiperSlide className={styles.swiperSlide}>Slide 3</SwiperSlide>
-      <SwiperSlide className={styles.swiperSlide}>Slide 4</SwiperSlide>
-    </Swiper>
+    <div>
+      <Swiper
+        // install Swiper modules
+        className={styles.containerSwiper}
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <div>
+          <SwiperSlide className={styles.sliderItem}>Slide 1</SwiperSlide>
+          <SwiperSlide className={styles.sliderItem}>Slide 2</SwiperSlide>
+          <SwiperSlide className={styles.sliderItem}>Slide 3</SwiperSlide>
+          <SwiperSlide className={styles.sliderItem}>Slide 4</SwiperSlide>
+        </div>
+      </Swiper>
+    </div>
   );
 };
+
